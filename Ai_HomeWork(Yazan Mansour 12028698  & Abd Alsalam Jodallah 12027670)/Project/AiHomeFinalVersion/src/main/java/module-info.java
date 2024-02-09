6d400a3854c3c942d8e7110065ed07a09687387c
AbdAlsalam Jodalah <129756251+abdalsalam-jodalah@users.noreply.github.com>
2024-02-10 00:48:03 +0200

    Delete Ai_HomeWork(Yazan Mansour 12028698  & Abd Alsalam Jodallah 12027670) directory

--- Ai_HomeWork(Yazan Mansour 12028698  & Abd Alsalam Jodallah 12027670)/Project/AiHomeFinalVersion/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.aihome {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.aihome to javafx.fxml;
-    exports com.example.aihome;
-}
